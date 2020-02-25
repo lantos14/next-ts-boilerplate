b8e7027133df40681d7058acc93d2ca4da20f550
Tibor Lantos <tibor.lantos@ericsson.com>
2020-02-25 09:31:26 +0100

    FEAT: ESLinter, Prettier

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'airbnb',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    'prettier',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    React: "writable",
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "prettier/prettier": "error",
+    "react/prop-types": "off",
+    "react/jsx-filename-extension": "off",
+    "@typescript-eslint/explicit-function-return-type": "off"
+  },
+};
